ff79da6f0dfd3963c72a5f92255997d2be813ce6
Luis del Cueto <luisdelcueto@Luiss-MacBook-Pro-2.local>
2017-06-22 17:21:48 -0400

    I can smell it

--- creating-text/js/index.js
+++ creating-text/js/index.js
@@ -1,140 +1,123 @@
-var camera, scence, renderer, cube, cube2, cube3, cub34, controls, ray
-var projector, mouse = { x: 0, y: 0 }, INTERSECTED; //mouse
+var info, renderer, scene, camera, controls, stats;
 
-init()
-animate()
+var nodes = []
+var raycaster;
+var mouse = new THREE.Vector2();
+var intersection;
+
+init();
+animate();
 
 function init() {
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+	var geometry, material, node, helper;
 
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-  camera.position.set(0, 0, 100);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
+	document.body.style.cssText = 'font: 600 12pt monospace; margin: 0; overflow: hidden' ;
 
-  scene = new THREE.Scene();
+	info = document.body.appendChild( document.createElement( 'div' ) );
+	info.style.cssText = 'left: 20px; position: absolute; top: 0px; width: 100% ';
+	info.innerHTML = '<div id=msg>Move the cursor to ID the cubes...</div>' + '';
 
-  var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(-20, 0, 20));
-  geometry.vertices.push(new THREE.Vector3(0, 20, 0));
-  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-  geometry.vertices.push(new THREE.Vector3(0, 20, 0));
-  geometry.vertices.push(new THREE.Vector3(20, 0, 0));
+	stats = new Stats();
+	stats.domElement.style.cssText = 'position: absolute; right: 0; top: 0; z-index: 100; ';
+	document.body.appendChild( stats.domElement );
 
-  var line = new THREE.Line(geometry, material);
+	renderer = new THREE.WebGLRenderer( { alpha: 1, antialias: true, clearColor: 0xffffff }  );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMapSoft = true;
+	document.body.appendChild( renderer.domElement );
 
-  var geometry = new THREE.BoxGeometry( 3, 3, 3 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  cube = new THREE.Mesh( geometry, material );
-  cube.position.x = -20
-  cube.position.y = 0
-  cube.position.z = 20
-  cube.updateMatrix()
+	scene = new THREE.Scene();
 
-  cube2 = new THREE.Mesh( geometry, material );
-  cube2.position.x = 0
-  cube2.position.y = 0
-  cube2.updateMatrix()
+	camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 5000 );
+	camera.position.set( 100, 100, 100 );
+	scene.add( camera ); // for light to follow
 
-  cube3 = new THREE.Mesh( geometry, material );
-  cube3.position.x = 0
-  cube3.position.y = 20
-  cube3.updateMatrix()
+	var domEvents	= new THREEx.DomEvents(camera, renderer.domElement)
+  // controls
+	controls = new THREE.OrbitControls( camera );
+	controls.minDistance = 10;
+	controls.maxDistance = 1000;
 
-  cube34 = new THREE.Mesh( geometry, material );
-  cube34.position.x = 20
-  cube34.position.y = 0
-  cube34.updateMatrix()
+  geometry = new THREE.BoxGeometry( 10, 10, 10 );
+
+	var node = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0x00ff00 } ) );
+	node.position.set(0, 0, 0);
+	node.castShadow = true;
+	node.receiveShadow = true;
+	scene.add( node );
+	nodes.push( node );
+	var node2 = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0x00ff00 } ) );
+	node.position.set(0, 20, 0);
+	node.castShadow = true;
+	node.receiveShadow = true;
+	scene.add( node2 );
+	nodes.push( node2 );
+
+  raycaster = new THREE.Raycaster();
 
 
-  scene.add(line, cube, cube2, cube3, cube34);
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	// domEvents.addEventListener(scene, 'click', function(event) {
+	// 	console.log('you clicked on the mesh')
+	// }, false)
+  document.addEventListener( 'click', clickOnNode, false );
+}
 
-  // initialize object to perform world/screen calculations
-	projector = new THREE.Projector();
+function clickOnNode() {
+	if (findIntersections() !== null) {
+		console.log(intersection.id); //REMEMBER TO GRAB BY NAME WHICH WILL BE OUR ID
+	}
+}
 
-	// when the mouse moves, call the given function
-	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+function onDocumentMouseMove( event ) {
 
-  function onDocumentMouseMove( event )
-  {
-  	// the following line would stop any other event handler from firing
-  	// (such as the mouse's TrackballControls)
-  	// event.preventDefault();
+	event.preventDefault();
 
-  	// update the mouse variable
-  	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-  	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-  }
-
-  //this is the users controller
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  camera.position.set(0, 0, 100); //position is the angle in which you look at something... z is how far and close you are
-  camera.up.set(0, 1, 0);
-  controls.update();
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
 }
 
-function animate() {
-  requestAnimationFrame( animate );
+function findIntersections() {
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  cube2.rotation.x += 0.01;
-  cube2.rotation.y += 0.01;
-  cube3.rotation.x += 0.01;
-  cube3.rotation.y += 0.01;
-  cube34.rotation.x += 0.01;
-  cube34.rotation.y += 0.01;
+		raycaster.setFromCamera( mouse, camera );
 
-  renderer.render(scene, camera);
-  update();
-}
-//sce
-function update()
-{
-	// find intersections
+		var intersections = raycaster.intersectObjects( nodes );
 
-	// create a Ray with origin at the mouse position
-	//   and direction into the scene (camera direction)
-	var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
-	projector.unprojectVector( vector, camera );
-	ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+		if ( intersections.length > 0 ) {
 
-	// create an array containing all objects in the scene with which the ray intersects
-	var intersects = ray.intersectObjects( scene.children );
+			if ( intersection != intersections[ 0 ].object ) {
 
-	// INTERSECTED = the object in the scene currently closest to the camera
-	//		and intersected by the Ray projected from the mouse position
+				intersection = intersections[ 0 ].object;
 
-	// if there is one (or more) intersections
-	if ( intersects.length > 0 )
-	{
-		// if the closest object intersected is not the currently stored intersection object
-		if ( intersects[ 0 ].object != INTERSECTED )
-		{
-		    // restore previous intersection object (if it exists) to its original color
-			if ( INTERSECTED )
-				INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
-			// store reference to closest object as current intersection object
-			INTERSECTED = intersects[ 0 ].object;
-			// store color of closest object (for later restoration)
-			INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
-			// set a new color for closest object
-			INTERSECTED.material.color.setHex( 0xffff00 );
+				msg.innerHTML = 'Selected cube ID: ' + intersection.id ;
+
+				if (nodes.find( (node) => node.id === intersection.id)) {
+					let found = nodes.find( (node) => node.id === intersection.id)
+					console.log(found);
+					found.material.color.setHex(0xffff000)
+					return intersection.id
+				}
+
+			}
+
+		} else {
+
+			intersection = null;
+			msg.innerHTML = 'No cube selected' ;
+			nodes.forEach((node) => node.material.color.setHex(0x00ff00))
+			return intersection
 		}
-	}
-	else // there are no intersections
-	{
-		// restore previous intersection object (if it exists) to its original color
-		if ( INTERSECTED )
-			INTERSECTED.material.color.setHex( INTERSECTED.currentHex );
-		// remove previous intersection object reference
-		//     by setting current intersection object to "nothing"
-		INTERSECTED = null;
-	}
 
+}
+
+
+function animate( timestamp ) {
+	renderer.render( scene, camera );
+	findIntersections();
 	controls.update();
+	stats.update();
+	requestAnimationFrame( animate );
 
 }
